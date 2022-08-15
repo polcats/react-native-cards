d2bde9746de114a9da56c03fbc6c5e25ee325b0e
polcats <24451518+polcats@users.noreply.github.com>
2022-08-15 17:39:49 +0800

    Fixed ViewPropTypes dependency

--- src/Touchable.js
+++ src/Touchable.js
@@ -1,53 +1,34 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import {
-  View,
-  TouchableOpacity,
-  TouchableNativeFeedback,
-  ViewPropTypes,
-} from 'react-native';
-import {
-  IS_ANDROID,
-  IS_LT_LOLLIPOP,
-  noop,
-} from './utils';
+import React from "react";
+import PropTypes from "prop-types";
+import { View, TouchableOpacity, TouchableNativeFeedback } from "react-native";
+import { IS_ANDROID, IS_LT_LOLLIPOP, noop } from "./utils";
+import { ViewPropTypes } from "deprecated-react-native-prop-types";
 
 const Touchable = ({ onPress, style, children }) => {
-  if (IS_ANDROID && !IS_LT_LOLLIPOP) {
-    return (
-      <TouchableNativeFeedback
-        background={TouchableNativeFeedback.SelectableBackground()}
-        onPress={onPress}
-      >
-        <View
-          style={style}
-        >
-          {children}
-        </View>
-      </TouchableNativeFeedback>
-    );
-  }
-  else {
-    return (
-      <TouchableOpacity
-        onPress={onPress}
-        style={style}
-      >
-        {children}
-      </TouchableOpacity>
-    );
-  }
+    if (IS_ANDROID && !IS_LT_LOLLIPOP) {
+        return (
+            <TouchableNativeFeedback background={TouchableNativeFeedback.SelectableBackground()} onPress={onPress}>
+                <View style={style}>{children}</View>
+            </TouchableNativeFeedback>
+        );
+    } else {
+        return (
+            <TouchableOpacity onPress={onPress} style={style}>
+                {children}
+            </TouchableOpacity>
+        );
+    }
 };
 
 Touchable.propTypes = {
-  onPress: PropTypes.func.isRequired,
-  style: ViewPropTypes.style,
-  children: PropTypes.node.isRequired,
+    onPress: PropTypes.func.isRequired,
+    style: ViewPropTypes.style,
+    children: PropTypes.node.isRequired,
 };
 
 Touchable.defaultProps = {
-  onPress: noop,
-  style: {}
+    onPress: noop,
+    style: {},
 };
 
 export default Touchable;
